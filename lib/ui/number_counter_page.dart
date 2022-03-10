7eacd6ab177decf870dfc9b9951103436d962262
nputrautama <nyoman.putra@ruangguru.com>
2022-03-10 21:50:13 +0800

    feat: add decrement counter

---
+++ lib/ui/number_counter_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class NumberCounterPage extends StatefulWidget {
+  const NumberCounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<NumberCounterPage> createState() => _NumberCounterPageState();
+}
+
+class _NumberCounterPageState extends State<NumberCounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("State Management Example"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
